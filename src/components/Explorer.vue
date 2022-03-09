b33141581df26ba1a2e8132ad58b10cfb70b5005
Nelson Fulfil <rockyxcoded@gmail.com>
2022-03-09 20:00:36 +0100

    Initial setup

---
+++ src/components/Explorer.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  transactions: Array
+})
+
+</script>
+
+<template>
+  <div class="py-10">
+    <div class="text-center">
+      <p
+        class="py-5"
+      >A simple Ethereum Blockchain Explorer that displays the last 10 transactions with selected details</p>
+    </div>
+
+    <div class="flex flex-col">
+      <div class="my-2 overflow-x-auto sm:mx-6 lg:mx-2">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-3">
+          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-200 px-10">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >Block Number</th>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >Address</th>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >Hash</th>
+                  <th
+                    scope="col"
+                    class="px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >Timestamp</th>
+                </tr>
+              </thead>
+              <tbody class="bg-white divide-y-2 divide-gray-100">
+                <tr>
+                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">answer.number</td>
+                  <td>answer.author</td>
+                  <td class="text-gray-600 text-sm">answer.hash</td>
+                  <td
+                    class="px-2 py-1 mt-3 inline-flex text-sm leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                  >answer.timestamp</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from 'vue'
+import Explorer from './Explorer.vue'
+
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <Explorer/>
+  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-xl w-full space-y-8 bg-white p-10 rounded-md shadow-sm">
+      <div>
+        <img
+          class="mx-auto h-12 w-auto"
+          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+          alt="Workflow"
+        />
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">ETH Address Watcher</h2>
+        <p class="mt-2 text-center text-sm text-gray-600">Watch Ethereum Address in realtime</p>
+      </div>
+      <form class="mt-8 space-y-6" action="#" method="POST">
+        <input type="hidden" name="remember" value="true" />
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Email address"
+            />
+          </div>
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-md font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >Watch Now</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
